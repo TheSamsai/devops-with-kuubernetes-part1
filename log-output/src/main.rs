a433f99743a5c08397782bcd495b6a26e3eca80e
Sami Lahtinen <samsai@posteo.net>
2022-06-19 13:09:19 +0300

    Updated log-output
    - Now has an HTTP endpoint for querying log information
    - Moved log output to an async task
    - Created service and ingress manifests

--- log-output/src/main.rs
+++ log-output/src/main.rs
@@ -4,14 +4,43 @@ use chrono::prelude::*;
 
 use std::{thread::sleep, time::Duration};
 
-fn main() {
+use std::net::SocketAddr;
+
+use axum::{
+    routing::get,
+    Router,
+};
+
+#[tokio::main]
+async fn main() {
     let string = Uuid::new_v4().hyphenated().to_string();
 
-    loop {
-        let time = Utc::now();
+    let port = std::env::var("PORT").map(|val| val.parse::<u16>().unwrap()).unwrap_or(3000);
 
-        println!("{}: {}", time, string);
+    let string_copy = string.clone();
+    let app = Router::new().route("/", get(|| async move { return_with_timestamp(&string_copy) }));
 
-        sleep(Duration::from_secs(5));
-    }
+    let addr = SocketAddr::from(([0,0,0,0], port));
+
+    println!("Started at port {}", port);
+
+    tokio::spawn(async move {
+        loop {
+            println!("{}", return_with_timestamp(&string));
+
+            tokio::time::sleep(Duration::from_secs(5)).await;
+        }
+    });
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+
+fn return_with_timestamp(value: &str) -> String {
+    let time = Utc::now();
+
+    format!("{}: {}", time, value)
 }
